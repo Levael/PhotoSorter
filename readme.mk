e2402de4e13cf4cdf7fd596992666ede403e9d87
Levael <Levael.Savvy@gmail.com>
2024-04-26 20:00:16 +0300

    hided lang btns for now; added TODO

--- readme.mk
+++ readme.mk
@@ -3,3 +3,11 @@ design: https://www.figma.com/file/cqubFbZaY5GySbd2kXFzLj/PhotoSorter-v0.2.4?typ
 notes:
 	- Panel Settings (in UiDocument) has parameter "Scale mode" which should be set to "Constant pixel size"
 	(by default is something else) to solve problem with different DPI (or, like in my case, in 125% scale from windows)
+
+todo:
+	- add GIF animator
+	- implement language change
+	- add VIDOE support
+	- somehow add 'bold' style inside TextElement strings
+	- clean and refactor codebase (especially UI related) (+thing about new data structure 'tuple to tuple dictionary')
+	- publish final version
