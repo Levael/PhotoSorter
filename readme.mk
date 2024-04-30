43aef5157b9bf433ac3a05fde2fc33c17157063f
Michael Levin <Levael.Savvy@gmail.com>
2024-04-30 18:22:39 +0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -4,20 +4,3 @@ design:
 notes:
 	- Panel Settings (in UiDocument) has parameter "Scale mode" which should be set to "Constant pixel size"
 	(by default is something else) to solve problem with different DPI (or, like in my case, in 125% scale from windows)
-
-bugs:
-	- have lags when loading big folder (maybe add loading bar)
-	- add chech for 'null' in 'DisplayImage'
-
-todo:
-	- make a release publish for final result only and delete Build folder from development
-	- add GIF, SVG, BMP add VIDEO support
-	- somehow add 'bold' style inside TextElement strings
-	- clean and refactor codebase (especially UI related) (+thing about new data structure 'tuple to tuple dictionary')
-	- publish final version
-	- redesign 'close' btn and add 'minimize' btn (like in windows)
-	- add 'type' to "file info"
-
-fixed / done:
-	- implement language change
-	- add check if there is folder from config (may be deleted)
