d064e5f3194398182bbfdac96e5bb60cbf77a573
Michael Levin <Levael.Savvy@gmail.com>
2024-04-30 16:46:11 +0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -1,17 +1,17 @@
-design: https://www.figma.com/file/cqubFbZaY5GySbd2kXFzLj/PhotoSorter-v0.2.4?type=design&node-id=9%3A115&mode=design&t=SMXlIHR8iHRglmUu-1
+design:
+	- https://www.figma.com/file/cqubFbZaY5GySbd2kXFzLj/PhotoSorter-v0.2.4?type=design&node-id=9%3A115&mode=design&t=SMXlIHR8iHRglmUu-1
 
 notes:
 	- Panel Settings (in UiDocument) has parameter "Scale mode" which should be set to "Constant pixel size"
 	(by default is something else) to solve problem with different DPI (or, like in my case, in 125% scale from windows)
 
 bugs:
-	- doesn't support .svg and .bmp
 	- have lags when loading big folder (maybe add loading bar)
 	- add chech for 'null' in 'DisplayImage'
 
 todo:
-	- add GIF animator
-	- add VIDEO support
+	- devide GIT repo to 2: development (with code only) and production (with built version only)
+	- add GIF, SVG, BMP add VIDEO support
 	- somehow add 'bold' style inside TextElement strings
 	- clean and refactor codebase (especially UI related) (+thing about new data structure 'tuple to tuple dictionary')
 	- publish final version
